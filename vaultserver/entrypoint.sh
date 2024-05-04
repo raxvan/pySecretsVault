bd28781859b7e3cd59495a3cf2f444550e909e9a
raxvan <razvan.om@gmail.com>
2024-05-04 09:30:15 -0700

    *

--- vaultserver/entrypoint.sh
+++ vaultserver/entrypoint.sh
@@ -16,7 +16,7 @@ if [ "$VAULT_SERVER_MODE" = "debug" ]; then
     #flask --app flask_server run
 else
     echo "VAULT: starting server with scaling $VAULT_SERVER_SCALING"
-	gunicorn -w $VAULT_SERVER_SCALING -b 0.0.0.0:5000 flask_server:app
+	gunicorn -w $VAULT_SERVER_SCALING -b $VAULT_HOST:$VAULT_PORT flask_server:app
     #gunicorn --worker-class sync --workers 4 --bind 0.0.0.0:8000 "myapp:create_app()" --pythonpath `which pypy3`
 fi
 
