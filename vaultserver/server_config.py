06927ee5a1a9de94c936d1dc08353207f042980a
raxvan <razvan.om@gmail.com>
2024-06-06 20:40:17 +0000

    * improved logs

--- vaultserver/server_config.py
+++ vaultserver/server_config.py
@@ -12,8 +12,8 @@ errorlog = _logdir + "/error.log"  # specify the path to the error log file
 logfile = _logdir + "/server.log"
 
 loglevel = "info"
-access_log_format = '%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+access_log_format = '%({X-Forwarded-For}i)s [%(h)s] %(l)s %(u)s %(t)s "%(r)s" -> {%(s)s/len(%(b)s)} "%(f)s"/"%(a)s"'
 
 # Worker processes
-timeout = 30
+timeout = 4
 keepalive = 2
